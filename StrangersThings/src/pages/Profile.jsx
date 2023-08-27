40d3a1f520152bc397aa95e8178d95a845594767
Talani Balogun <Taloni5992@gmail.com>
2023-08-27 11:57:42 -0400

    initial files

--- StrangersThings/src/pages/Profile.jsx
+++ StrangersThings/src/pages/Profile.jsx
@@ -6,6 +6,16 @@ export default  function Profile({ token }) {
     return (
         <div>
         <h1>Profile</h1>
-        </div>
+        <button>Create New Message</button>
+       <form>
+        <h2>Recent Messages</h2> 
+
+       </form>
+       <form>
+        <h2>Sent Messages</h2>
+       </form>
+       </div>
+
+        
     )
 }
